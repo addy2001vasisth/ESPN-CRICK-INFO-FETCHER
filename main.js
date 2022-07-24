23f08809718b72af5d2b9585a98147111c0f4ab1
addy2001vasisth <addy24012001@gmail.com>
2022-07-24 11:41:07 +0530

    changes undo

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const url = "https://www.espncricinfo.com/series/indian-premier-league-2022-1298423";
-// https://www.espncricinfo.com/series/ipl-2021-1249214
+const url = "https://www.espncricinfo.com/series/ipl-2021-1249214";
 const req = require("request");
 const cheerio = require("cheerio");
 const aM = require("./allMatches.js");
@@ -7,33 +6,24 @@ const { default: DomHandler } = require("domhandler");
 const fs = require("fs");
 const path = require("path");
 
-// if (fs.existsSync("IPL") == false) {
-//   fs.mkdirSync("IPL");
-// }
+if (fs.existsSync("IPL") == false) {
+  fs.mkdirSync("IPL");
+}
 
-// req(url, function (err, res, html) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     handle(html);
-//   }
-// });
+req(url, function (err, res, html) {
+  if (err) {
+    console.log(err);
+  } else {
+    handle(html);
+  }
+});
 
-// function handle(html) {
-//   let $ = cheerio.load(html);
-//   let allMatchUrl = $(".widget-items.cta-link a").attr("href");
-//   allMatchUrl = "https://www.espncricinfo.com" + allMatchUrl;
-//   aM.am(allMatchUrl);
-// }
+function handle(html) {
+  let $ = cheerio.load(html);
+  let allMatchUrl = $(".widget-items.cta-link a").attr("href");
+  allMatchUrl = "https://www.espncricinfo.com" + allMatchUrl;
+  aM.am(allMatchUrl);
+}
 
 
   
-req(url,cb)
-function cb(err,request,html){
-  if(err){
-    console.log(err);
-  }
-  else{
-    console.log(html);
-  }
-}
